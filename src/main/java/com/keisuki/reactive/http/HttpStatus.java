556cc1db772e2ddab080ad2f25c3f61c12e7ada9
Keisuki <willofthestars@gmail.com>
2021-04-27 18:19:48 +0100

    Add some additional response status codes

--- src/main/java/com/keisuki/reactive/http/HttpStatus.java
+++ src/main/java/com/keisuki/reactive/http/HttpStatus.java
@@ -1,7 +1,12 @@
 package com.keisuki.reactive.http;
 
 public enum HttpStatus {
-  OK(200, "OK");
+  OK(200, "OK"),
+  NO_CONTENT(204, "No Content"),
+  BAD_REQUEST(400, "Bad Request"),
+  NOT_FOUND(404, "Not Found"),
+  INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+  SERVICE_UNAVAILABLE(503, "Service Unavailable");
 
   int statusCode;
   String message;
